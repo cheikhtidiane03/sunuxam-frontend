2be296bee570500cdb4027c6c1282392ce64bdf6
Cheikh Tidiane Ba <cheikhtidianeb764@gmail.com>
2026-08-16 18:59:11 +0000

    environnemnt de production

--- src/app/core/services/api-config.ts
+++ src/app/core/services/api-config.ts
@@ -1,1 +1,5 @@
-export const API_BASE_URL = 'http://localhost:8090/api';
+import { environment } from '../../../environments/environment';
+
+export const API_BASE_URL = environment.apiBaseUrl;
+
+//export const API_BASE_URL = 'http://localhost:8090/api';
